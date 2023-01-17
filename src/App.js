34757f2fb03199aa313f6b7e9319288b4cd8e9ee
WILSONPLUR <yourshows12@gmail.com>
2023-01-18 00:05:33 +0300

    frontend for chat without good ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// @ts-nocheck
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3005");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const [room, setRoom] = useState("");
+  // const [writtenBy, setWrittenBy] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+  const joinRoom = () => {
+    if (room !== "") socket.emit("join_room", room);
+  };
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <input
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          placeholder="join room"
+        />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Message..."
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Chat: </h1>
+      <span>{messageReceived}</span>
     </div>
   );
 }
